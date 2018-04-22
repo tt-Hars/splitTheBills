a5bbcdad9af4b9639f1b4a125d0e2f1c58484008
harshit <harshit@email.id>
2018-04-22 18:41:09 +0530

    event save complete

--- src/app/add-user-events/add-user-events.component.ts
+++ src/app/add-user-events/add-user-events.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, OnChanges, Output,
-  EventEmitter, Input, Directive, ElementRef, Inject, Renderer2, Renderer} from '@angular/core';
+import {
+  Component, OnInit, OnChanges, Output,
+  EventEmitter, Input, Directive, ElementRef, Inject, Renderer2, Renderer
+} from '@angular/core';
 import { fail } from 'assert';
-import {UserDetails, EventDetails} from '../ifsc';
+import { UserDetails, EventDetails } from '../ifsc';
 
 declare var M;
 @Component({
@@ -23,7 +25,7 @@ export class AddUserEventsComponent implements OnChanges, OnInit {
   eventDetails: EventDetails[] = [];
   eventDetailsList: EventDetails[] = [];
   userAddWarningMsg = '';
-  isUserListHidden =true;
+  isUserListHidden = true;
   userListShowHideText = 'Show all';
   addUserclicked() {
     this.isAddUsersFormEleHidden = false;
@@ -34,17 +36,17 @@ export class AddUserEventsComponent implements OnChanges, OnInit {
   }
 
   showEventsBox() {
-   this.isAddUsersFormEleVisible = true;
-   this.isAddUsersFormEleHidden = false;
-   this.isAddEventsFormEleVisible = true;
-   this.isAddEventsFormEleHidden = false;
-   this.isFromEvents = true;
+    this.isAddUsersFormEleVisible = true;
+    this.isAddUsersFormEleHidden = false;
+    this.isAddEventsFormEleVisible = true;
+    this.isAddEventsFormEleHidden = false;
+    this.isFromEvents = true;
 
-   const elem = document.querySelector('.modal');
-   console.log(elem);
-   const options = {};
-   const instance = M.Modal.init(elem, options);
-   instance.open();
+    const elem = document.querySelector('.modal');
+    console.log(elem);
+    const options = {};
+    const instance = M.Modal.init(elem, options);
+    instance.open();
   }
 
   delUserFromUserDetails(val) {
@@ -61,7 +63,7 @@ export class AddUserEventsComponent implements OnChanges, OnInit {
   addUserToUserDetails(val) {
     const element = this._renderer.selectRootElement('.userInfo');
     if (document.getElementById('addUserIpt').className.indexOf('invalid') < 0) {
-      this.userDetails.push({or_id: this.userCounter, name: val, new_id: this.userCounter} );
+      this.userDetails.push({ or_id: this.userCounter, name: val, new_id: this.userCounter });
       this.userValue = '';
       this.userCounter++;
       this.refreshArrayIds();
@@ -69,16 +71,84 @@ export class AddUserEventsComponent implements OnChanges, OnInit {
     } else {
       this.userAddWarningMsg = 'Please provide input';
     }
-      element.value = '';
-      setTimeout(() => element.focus(), 0);
+    element.value = '';
+    setTimeout(() => element.focus(), 0);
   }
 
-  addEventToEventDetails() {
+  addEventToEventDetails(evName, evDesc, evTotAmt, evDate) {
+    let indvAmt = [];
+    let indvIds = [];
+    let indvActive = [];
+    let or_id = 0;
+    let new_id = 0;
+    /*const indvAmtNodeList = document.querySelectorAll('.indvAmntCont');   //Array.from
+    const indvActiveNodeList = document.querySelectorAll('.indvActive');
+    const indvDetNodeList = document.querySelectorAll('.usrDet');*/
+   const indvAmtNodeList = Array.from(document.querySelectorAll('.indvAmntCont'));   //Array.from
+   const indvActiveNodeList = Array.from(document.querySelectorAll('.indvActive'));
+   const indvDetNodeList = Array.from(document.querySelectorAll('.usrDet'));
+
+   
+    Array.prototype.forEach.call(indvAmtNodeList, function (currentValue, currentIndex) {
+      indvAmt.push({ val: currentValue.value, id: currentIndex })
+    });
+    Array.prototype.forEach.call(indvActiveNodeList, function (currentValue, currentIndex) {
+      indvActive.push({ val: currentValue.checked, id: currentIndex })
+    });
+    Array.prototype.forEach.call(indvDetNodeList, function (currentValue, currentIndex) {
+      const getId = currentValue.innerText.substring(0,1);
+      indvIds.push({ val: getId, id: currentIndex })
+    });
+
+    /*indvAmtNodeList.forEach( (currentValue, currentIndex) => {
+      indvAmt.push({ val: currentValue.value, id: currentIndex })
+    });
+
+    indvActiveNodeList.forEach( (currentValue, currentIndex) => {
+      indvActive.push({ val: currentValue.checked, id: currentIndex })
+    });
+
+    indvDetNodeList.forEach( (currentValue, currentIndex) => {
+      const getId = currentValue.innerText.substring(0,1);
+      indvIds.push({ val: getId, id: currentIndex })
+    });*/
+
+    const resArray = this.createUsersListArray(indvIds, indvAmt, indvActive);
     
+
+    //console.log(evName, evDesc, evTotAmt, evDate);
+    //console.log(indvAmt, indvActive);
+    this.eventDetails.push({or_id, new_id,event_name: evName,event_tot_amt: evTotAmt,event_desc: evDesc,event_date: evDate,users_desc: resArray});
+    or_id++;
+    new_id = or_id;
+    console.log(this.eventDetails);
+  }
+
+  createUsersListArray(arr1, arr2, arr3) : any{
+    let result = [{}];
+    for(let i = 0; i< arr1.length; i++){
+      if(i == arr1[i].id && i == arr2[i].id && i == arr3[i].id){
+        result.push({id:arr1[i].val, amount_cont:arr2[i].val, user_actve:arr3[i].val})
+      }
+    }
+    return result;
   }
 
   clearFields() {
-
+    const indvAmtNodeList = Array.from(document.querySelectorAll('.indvAmntCont'));   //Array.from
+    const indvActiveNodeList = Array.from(document.querySelectorAll('.indvActive'));
+    const indvDetNodeList = Array.from(document.querySelectorAll('.usrDet'));
+ 
+    Array.prototype.forEach.call(indvAmtNodeList, function (currentValue, currentIndex) {
+      currentValue.value = 0;
+    });
+    Array.prototype.forEach.call(indvActiveNodeList, function (currentValue, currentIndex) {
+      currentValue.checked = false;
+    });
+    (<HTMLInputElement>document.getElementById('eventName')).value='';
+    (<HTMLInputElement>document.getElementById('eventDesc')).value='';
+    (<HTMLInputElement>document.getElementById('totAmntCont')).value = '0';
+    (<HTMLInputElement>document.getElementById('evDate')).value='';
   }
 
   openCal() {
@@ -88,7 +158,7 @@ export class AddUserEventsComponent implements OnChanges, OnInit {
     instance.open();
   }
 
-  showUsersInEvent(){
+  showUsersInEvent() {
     this.isUserListHidden = this.isUserListHidden ? false : true;
     this.userListShowHideText = this.isUserListHidden ? 'Show all ' : 'Hide '
   }
--- src/app/ifsc.ts
+++ src/app/ifsc.ts
@@ -8,11 +8,14 @@ export interface EventDetails {
     or_id: number;
     new_id: number;
     event_name: string;
+    event_tot_amt: number;
     event_desc: string;
-    users_desc: {
-        id: number;
-        name: string;
-        amount_cont: number;
-    };
     event_date: string;
+    users_desc : [{
+        id: number;
+        //name: string;
+        amount_cont: number;
+        user_actve: boolean;
+    }];
+    //users_desc: any;
 }
