d757788938040c344566ac63b6b0a647b119c03e
h.t <h@t.co>
2023-08-13 12:52:19 +0530

    moved from less to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { AddUserEventsComponent } from './add-user-events/add-user-events.component';
-import { DataServiceService } from './data-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
-  constructor(private _ds: DataServiceService) {}
-  title = 'Split your bills';
-  currentUser = 'There';
-  imgPath= './app/images/dummy-ser.png';
-  isEleVisible = true;
-  isEleHidden = true;
-  getUserName(userName: string): void {
-    if (userName !== null) {
-    this.currentUser = userName;
-    this.isEleHidden = true;
-    }
-  }
+  title=''
+  
   ngOnInit() {
-    this._ds.currentName.subscribe(message => this.currentUser = message);
+this.title = 'hello world'
   }
 }
